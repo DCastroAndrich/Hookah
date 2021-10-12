50ebd42c9db05e1f481a2037f3339ca06a738ef6
Damian <castroandrich.damian@gmail.com>
2021-10-12 19:17:08 -0300

    Install React-Bootstrap, Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const NavBar = () => {
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark" sticky="top" >
+        <Container>
+          <Navbar.Brand href="#home">Krishna Hooka's</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#hooka">Hooka</Nav.Link>
+              <Nav.Link href="#tabacos">Tabacos</Nav.Link>
+              <Nav.Link href="#carbones">Carbones</Nav.Link>
+              <Nav.Link href="#accesorios">Accesorios</Nav.Link>
+              <Nav.Link href="#contacto">Contacto</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default NavBar
